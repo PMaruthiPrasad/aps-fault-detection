f3fc641e44d0eae7b25de1eba7b50d17bae3ce22
P Maruthi Prad <pmaruthiprasad16@gmail.com>
2022-12-20 06:53:14 +0530

    data validation about to complete

--- sensor/logger.py
+++ sensor/logger.py
@@ -1,49 +1,23 @@
-from sensor.entity import artifact_entity,config_entity
-from sensor.exception import SensorException
-from sensor.logger import logging
-from scipy.stats import ks_2samp
-from typing import Optional
-import os,sys 
-import pandas as pd
-from sensor import utils
-import numpy as np
-from sensor.config import TARGET_COLUMN
+import logging
+import os
+from datetime import datetime
+import os
 
+#log file name
+LOG_FILE_NAME = f"{datetime.now().strftime('%m%d%Y__%H%M%S')}.log"
 
+#log directory
+LOG_FILE_DIR = os.path.join(os.getcwd(),"logs")
 
+#create folder if not available
+os.makedirs(LOG_FILE_DIR,exist_ok=True)
 
-class DataValidation:
+#log file path
 
+LOG_FILE_PATH = os.path.join(LOG_FILE_DIR,LOG_FILE_NAME)
 
-    def __init__(self,
-                    data_validation_config:config_entity.DataValidationConfig,
-                    data_ingestion_artifact:artifact_entity.DataIngestionArtifact):
-        try:
-            logging.info(f"{'>>'*20} Data Validation {'<<'*20}")
-            self.data_validation_config = data_validation_config
-            self.data_ingestion_artifact=data_ingestion_artifact
-            self.validation_error=dict()
-        except Exception as e:
-            raise SensorException(e, sys)
-
-    
-
-    def drop_missing_values_columns(self,df:pd.DataFrame,report_key_name:str)->Optional[pd.DataFrame]:
-        """
-        This function will drop column which contains missing value more than specified threshold
-        df: Accepts a pandas dataframe
-        threshold: Percentage criteria to drop a column
-        =====================================================================================
-        returns Pandas DataFrame if atleast a single column is available after missing columns drop else None
-        """
-        try:
-            
-            threshold = self.data_validation_config.missing_threshold
-            null_report = df.isna().sum()/df.shape[0]
-            #selecting column name which contains null
-            logging.info(f"selecting column name which contains null above to {threshold}")
-            drop_column_names = null_report[null_report>threshold].index
-
-            logging.info(f"Columns to drop: {list(drop_column_names)}")
-            self.validation_error[report_key_name]=list(drop_column_names)
-            df.drop(list(drop_column_names),axis=1,inplace=True)
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+)
